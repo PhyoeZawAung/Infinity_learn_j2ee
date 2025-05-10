29190a4d01ce90ad2496cbf3f5c9bddfa9eeaf77
Phyoe Zaw Aung <phyoezawaung9696@gmail.com>
2025-05-10 10:32:21 +0630

    create guest filter

---
+++ src/main/java/uni/project/infinitylearn/filters/GuestFilter.java
@@ -0,0 +1,44 @@
+package uni.project.infinitylearn.filters;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import uni.project.infinitylearn.models.User;
+
+public class GuestFilter implements Filter {
+
+    
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        HttpSession session = req.getSession(false);
+
+        if(session != null) {
+            res.sendRedirect("/dashboard");
+        }
+        
+        chain.doFilter(request, response); // allow through
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {}
+
+    @Override
+    public void destroy() {}
+    
+}
